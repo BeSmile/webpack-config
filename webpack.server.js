8f8967ec96d4e54a9fa5001c8cfbf4a043d24abf
Smile <l735465519@gmail.com>
2019-09-22 16:08:29 +0800

    add ts

--- webpack.server.js
+++ webpack.server.js
@@ -1,23 +1,26 @@
 var path = require("path");
 var fs = require('fs')
-var rules = require("./lib/rule.js");
+var { getRule } = require("./lib/rule.js");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CheckerPlugin } = require('awesome-typescript-loader');
 
+
+const tsLoader = process.argv.includes('ts-loader');
 const modelPath = path.resolve(__dirname, '..', 'src', 'models');
 
 function getModel(modelPath) {
     var models = [];
 
     return new Promise((resolve, reject) => {
-        fs.readdir(modelPath, function (err, files) {
-          if (err) {
-            resolve([]);
-          }
-          files.forEach(function(filename){
-              const res = filename.split(".");
-              models.push(res[0]);
-          });
-          resolve(models);
+        fs.readdir(modelPath, function(err, files) {
+            if (err) {
+                resolve([]);
+            }
+            files.forEach(function(filename) {
+                const res = filename.split(".");
+                models.push(res[0]);
+            });
+            resolve(models);
         });
     })
 }
@@ -27,76 +30,79 @@ async function renderWebpack() {
 
     const models = await getModel(modelPath);
     var webpack = {
-      mode: "development",
-      resolve: {
-        extensions: ['.js', '.jsx', '.json'],
-        alias: {
-          '@src': path.resolve(__dirname, '..', "src/"),
-          '@pages': path.resolve(__dirname, '..', 'src', "pages/"),
-          '@components': path.resolve(__dirname, '..', 'src', "components/"),
-          '@atom': path.resolve(__dirname, '..', 'src', "atom/"),
-          '@public': path.resolve(__dirname, '..', "public/"),
+        mode: "development",
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+            alias: {
+                '@src': path.resolve(__dirname, '..', "src/"),
+                '@pages': path.resolve(__dirname, '..', 'src', "pages/"),
+                '@components': path.resolve(__dirname, '..', 'src', "components/"),
+                '@atom': path.resolve(__dirname, '..', 'src', "atom/"),
+                '@public': path.resolve(__dirname, '..', "public/"),
+            },
         },
-      },
-      entry: {
-        app: path.resolve(__dirname, '..', 'src','index.js')
-      },
-      output: {
-          path: path.resolve(__dirname, '.', 'dist'), // 输出的路径
-          filename: '[name]-[chunkhash].js',
-          // publicPath: "/assets/",
-          library: 'APP',
-          libraryTarget: 'window',
-      },
-      resolveLoader: {
-        alias: {
-          // "db-loader": path.resolve(__dirname, '.','babel-loader.js')
-        }
-      },
-      devtool: 'source-map',
-      module: {
-        rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
+        entry: {
+            app: path.resolve(__dirname, '..', 'src', 'index.tsx')
+        },
+        output: {
+            path: path.resolve(__dirname, '.', 'dist'), // 输出的路径
+            filename: '[name]-[chunkhash].js',
+            // publicPath: "/assets/",
+            library: 'APP',
+            libraryTarget: 'window',
+        },
+        // externals: {
+        //     "react": "React",
+        //     "react-dom": "ReactDOM"
+        // },
+        resolveLoader: {
+            alias: {
+                // "db-loader": path.resolve(__dirname, '.','babel-loader.js')
             }
-          },
-          ...rules,
-          // {
-          //   test: /\.html$/,
-          //   use: [
-          //     {
-          //       loader: "html-loader"
-          //     }
-          //   ]
-          // }
-        ]
-      },
-      target: "web",
-      plugins: [
-        new webpack.DefinePlugin({
-            'process.env': 'dev',
-            IS_DEV: JSON.stringify(false),
-            MODELS: JSON.stringify(models),
-        }),
-        new HtmlWebpackPlugin({
-           chunks: ['app'],//限定entry特定的块
-           excludeChunks: ['dev-helper'], //排除entry特定的块
-           filename: 'index.html',
-           inject: true,
-           hash: true,
-           mountPoint: '<div id="root"></div>',
-           // value: '23',
-           template: path.resolve('.', 'public', 'index.html')  // 模板
-        })
-      ],
-      devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        compress: true,
-        port: 9000
-      }
+        },
+        devtool: 'source-map',
+        module: {
+            rules: [{
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                ...getRule(tsLoader),
+                // {
+                //   test: /\.html$/,
+                //   use: [
+                //     {
+                //       loader: "html-loader"
+                //     }
+                //   ]
+                // }
+            ]
+        },
+        target: "web",
+        plugins: [
+            new webpack.DefinePlugin({
+                ENV_PATH: JSON.stringify(false),
+                MODELS_PATH: JSON.stringify(models),
+            }),
+            // new CheckerPlugin(),
+            new HtmlWebpackPlugin({
+                chunks: ['app'], //限定entry特定的块
+                excludeChunks: ['dev-helper'], //排除entry特定的块
+                filename: 'index.html',
+                inject: true,
+                hash: true,
+                mountPoint: '<div id="root"></div>',
+                // value: '23',
+                template: path.resolve('.', 'public', 'index.html') // 模板
+            })
+        ],
+        devServer: {
+            contentBase: path.join(__dirname, "dist"),
+            compress: true,
+            port: 9000
+        }
     };
     return webpack;
 }
