dd479de2cd53c85164a85c79027b1a0f1e40af5f
Smile <l735465519@gmail.com>
2021-12-14 13:59:06 +0800

    chore: webpack重构

--- webpack.server.js
+++ webpack.server.js
@@ -1,8 +1,8 @@
 var path = require("path");
 var fs = require('fs')
-var {
-    getRule
-} = require("./lib/rule.js");
+var rules = require('./lib/rule.js');
+var getPlugins = require('./lib/plugins');
+
 // var child_process = require('child_process');
 
 var HtmlWebpackPlugin = require("html-webpack-plugin");
@@ -12,10 +12,18 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 const {
     CheckerPlugin
 } = require('awesome-typescript-loader');
-var config = require('../.gents.ts');
-const tsLoader = process.argv.includes('ts-loader');
+
+var config = {};
+
+if (process.env.NODE_MODE !== 'plugin') {
+  config = require('../.gents.ts');
+}
+
+
+// var config = require('../.gents.ts');
+const tsLoader = process.env.NODE_TS;
 const modelPath = path.resolve(__dirname, '..', 'src', 'models');
-let tm2 = null;
+
 
 // fs.watch(modelPath, function (event, filename) {
 //   if(event === 'rename') {
@@ -34,53 +42,18 @@ let tm2 = null;
 //   } 
 // });
 
-function getModel(modelPath) {
-    var models = [];
+console.log(path.join(__dirname, '..', "public"));
 
-    return new Promise((resolve, reject) => {
-        fs.readdir(modelPath, function(err, files) {
-            if (err) {
-                resolve([]);
-            }
-            files.filter(fileName => fileName.indexOf('d.ts') < 0).forEach(function(filename) {
-                const res = filename.split(".");
-                models.push(res[0]);
-            });
-            resolve(models);
-        });
-    })
-}
-
+// 生成webpack文件
 async function renderWebpack() {
     var webpack = require("webpack");
-    const models = await getModel(modelPath);
+    const plugins = await getPlugins();
+
     var webpack = {
-        stats: { 
-          children: false,
-          chunks: false,
-          hash: false,
-          warnings: false,
-          builtAt: false,
-          modules: false,
-          timings: false,
-          assets: false,
-          entrypoints: false,
-        },
+        stats: "none",// 错误提示
         mode: "development",
         resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
-            // alias: {
-            //     '@src': path.resolve(__dirname, '..', "src"),
-            //     '@layouts': path.resolve(__dirname, '..', "src", 'layouts'),
-            //     '@pages': path.resolve(__dirname, '..', 'src', "pages"),
-            //     '@components': path.resolve(__dirname, '..', 'src', "components"),
-            //     '@services': path.resolve(__dirname, '..', 'src', "services"),
-            //     '@assets': path.resolve(__dirname, "../src/assets"),
-            //     '@atom': path.resolve(__dirname, '..', 'src', "atom"),
-            //     '@utils': path.resolve(__dirname, '..', 'src', "utils"),
-            //     '@models': path.resolve(__dirname, '..', 'src', "models"),
-            //     '@public': path.resolve(__dirname, '..', "public"),
-            // },
+            extensions: ['.js', '.jsx','.ts', '.tsx',  '.json'],
             alias: config.alias,
             // plugins: [
             //   new TsconfigPathsPlugin({
@@ -93,12 +66,12 @@ async function renderWebpack() {
             // ]
         },
         entry: {
-            app: path.resolve(__dirname, '..', 'src', tsLoader ? 'index.tsx' : 'index.js')
+            app: path.resolve(__dirname, '..', 'src', tsLoader ? 'index.tsx' : 'index.js'),
         },
         output: {
             path: path.resolve(__dirname, '.', 'dist'), // 输出的路径
             filename: '[name]-[chunkhash].js',
-            // publicPath: "/assets/",
+            // publicPath: path.resolve(__dirname, '..', 'public'),
             library: 'APP',
             libraryTarget: 'window',
         },
@@ -111,52 +84,40 @@ async function renderWebpack() {
                 // "db-loader": path.resolve(__dirname, '.','babel-loader.js')
             }
         },
-        devtool: 'source-map',
+        devtool: 'eval-source-map',
         module: {
-            rules: [
-                ...getRule({
-                    ts: tsLoader
-                }),
-                {
-                    test: /\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader"
-                    }
-                },
-
-                // {
-                //   test: /\.html$/,
-                //   use: [
-                //     {
-                //       loader: "html-loader"
-                //     }
-                //   ]
-                // }
-            ]
+          rules: rules,
         },
-        target: "web",
-        plugins: [
-            new webpack.DefinePlugin({
-                ENV_PATH: JSON.stringify(false),
-                MODELS_PATH: JSON.stringify(models),
-            }),
-            // new CheckerPlugin(),
-            new HtmlWebpackPlugin({
-                chunks: ['app'], //限定entry特定的块
-                excludeChunks: ['dev-helper'], //排除entry特定的块
-                filename: 'index.html',
-                inject: true,
-                hash: true,
-                mountPoint: '<div id="root"></div>',
-                // value: '23',
-                template: path.resolve('.', 'public', 'document.ejs') // 模板
-            })
-        ],
+        target: 'web',
+        // plugins: [
+        //     new webpack.DefinePlugin({
+        //         ENV_PATH: JSON.stringify(false),
+        //         MODELS_PATH: JSON.stringify(models),
+        //     }),
+        //     // new CheckerPlugin(),
+        //     new HtmlWebpackPlugin({
+        //         chunks: ['app'], //限定entry特定的块
+        //         excludeChunks: ['dev-helper'], //排除entry特定的块
+        //         filename: 'index.html',
+        //         inject: true,
+        //         hash: true,
+        //         mountPoint: '<div id="root"></div>',
+        //         // value: '23',
+        //         template: path.resolve('.', 'public', 'document.ejs') // 模板
+        //     })
+        // ],
+        plugins: plugins,
         devServer: {
-            contentBase: path.join(__dirname, "dist"),
+            host:'127.0.0.1',
+            contentBase: path.join(__dirname, '..', "public"),// 用于指定资源目录
             compress: true,
-            port: 9000
+            port: 9000,
+            clientLogLevel: 'silent',
+            // before: function (app, server, compiler) {
+            //   app.get('/some/path', function (req, res) {
+            //     res.json({ custom: 'response' });
+            //   });
+            // }
         },
         // externals: {
         //     "react": 'react',
